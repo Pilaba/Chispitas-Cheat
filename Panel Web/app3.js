4d4f428276e9502b32a8692e82ea75c1fdd0e015
Baldemar <pilabapoche@gmail.com>
2019-03-24 13:32:02 -0600

    fix https

--- Panel Web/app3.js
+++ Panel Web/app3.js
@@ -7,7 +7,7 @@ var options = {
 
 //Web setup
 const app = require("express")();
-const http = require('http').createServer(options, app);
+const http = require('https').createServer(options, app);
 const io = require("socket.io").listen(http);
 
 //Dengurs - Autorizar sitios https con certificado caduco o auto firmado
@@ -302,7 +302,7 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
 
-var port = process.env.PORT || 80
+var port = process.env.PORT || 443
 http.listen(port, function() {
     console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
